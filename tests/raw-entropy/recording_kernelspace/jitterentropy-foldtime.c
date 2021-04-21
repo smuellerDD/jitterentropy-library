a0c51e21c2d4322681a320a22de5e2ef13c08196
Stephan Mueller <smueller@chronox.de>
2021-04-21 13:39:48 +0200

    apply copyright date bump
    Signed-off-by: Stephan Mueller <smueller@chronox.de>

--- tests/raw-entropy/recording_kernelspace/jitterentropy-foldtime.c
+++ tests/raw-entropy/recording_kernelspace/jitterentropy-foldtime.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019, Stephan Mueller <smueller@chronox.de>
+ * Copyright (C) 2019 - 2021, Stephan Mueller <smueller@chronox.de>
  *
  * License: see LICENSE file in root directory
  *
