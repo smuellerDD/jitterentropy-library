62be107ef2e53a2e3359ccab27e54bf71109a495
Stephan Mueller <smueller@chronox.de>
2021-10-24 21:51:56 +0200

    JENT: fix warning on mingw64
    Compiling with the mingw64 compiler for x86_64 targets gives a warning:
    ```
    src/jitterentropy-base-user.h:100:58: warning: left shift count >= width of type [-Wshift-count-overflow]
     # define EAX_EDX_VAL(val, low, high)     ((low) | (high) << 32)
    ```
    
    Reported-by: ronaaron
    Signed-off-by: Stephan Mueller <smueller@chronox.de>

--- jitterentropy-base-user.h
+++ jitterentropy-base-user.h
@@ -96,7 +96,7 @@
 
 #ifdef __x86_64__
 
-# define DECLARE_ARGS(val, low, high)    unsigned long low, high
+# define DECLARE_ARGS(val, low, high)    uint64_t  low, high
 # define EAX_EDX_VAL(val, low, high)     ((low) | (high) << 32)
 # define EAX_EDX_RET(val, low, high)     "=a" (low), "=d" (high)
 
