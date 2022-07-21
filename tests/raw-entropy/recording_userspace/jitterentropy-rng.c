51aa7a5d8165c76358d4cf96649c346f06e0e370
Orgad Shaneh <orgad.shaneh@audiocodes.com>
2022-07-21 11:07:30 +0300

    Avoid C++ style comments

--- tests/raw-entropy/recording_userspace/jitterentropy-rng.c
+++ tests/raw-entropy/recording_userspace/jitterentropy-rng.c
@@ -78,7 +78,7 @@ int main(int argc, char * argv[])
 			val = strtoul(argv[1], NULL, 10);
 			switch (val) {
 			case 0:
-				// Allow to set no option
+				/* Allow to set no option */
 				break;
 			case 1:
 				flags |= JENT_MAX_MEMSIZE_32kB;
