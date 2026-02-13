f4184f35d994bf2e42cfd3128fedde5ffe221a22
Markus Theil <theil.markus@gmail.com>
2026-02-13 09:36:57 +0100

    status: format as json
    Signed-off-by: Markus Theil <theil.markus@gmail.com>

--- src/jitterentropy-status.c
+++ src/jitterentropy-status.c
@@ -23,116 +23,116 @@
 #include "jitterentropy-base.h"
 #include "jitterentropy-internal.h"
 
+/*
+ * Always validate the output with something like "jq -e .", when doing changes here.
+ *
+ * No JSON library is used here to keep dependencies slim and this is serialized only
+ * here.
+ */
 int jent_status(const struct rand_data *ec, char *buf, size_t buflen)
 {
 	size_t used;
 
-	snprintf(buf, buflen, "Jitter RNG version: %u.%u.%u\n\n",
-		 JENT_MAJVERSION, JENT_MINVERSION, JENT_PATCHLEVEL);
+	#define jent_add_to_status(...)					\
+	{								\
+		used = strlen(buf);					\
+		snprintf(buf + used, buflen - used, __VA_ARGS__);	\
+	}
 
-	used = strlen(buf);
-	snprintf(buf + used, buflen - used, "Health Status:\n");
+	/* needed as plain snprintf to make jent_add_to_status len calculation usable */
+	snprintf(buf, buflen, "{\n");
 
-	used = strlen(buf);
-	snprintf(buf + used, buflen - used,
-		 " APT:\n  intermittent: %s\n  permanent: %s\n",
-		 ec->health_failure & JENT_APT_FAILURE ? "fail" : "pass",
-		 ec->health_failure & JENT_APT_FAILURE_PERMANENT ? "fail" :
-								   "pass");
-	used = strlen(buf);
-	snprintf(buf + used, buflen - used,
-		 " RCT:\n  intermittent: %s\n  permanent: %s\n",
-		 ec->health_failure & JENT_RCT_FAILURE ? "fail" : "pass",
-		 ec->health_failure & JENT_RCT_FAILURE_PERMANENT ? "fail" :
-								   "pass");
+	jent_add_to_status("\t\"version\": \"%u.%u.%u\",\n",
+			   JENT_MAJVERSION, JENT_MINVERSION, JENT_PATCHLEVEL)
+
+	/*
+	 * health
+	 */
+	jent_add_to_status("\t\"healthFailure\": {\n");
+
+	jent_add_to_status("\t\t\"apt\": {\n");
+	jent_add_to_status("\t\t\t\"intermittent\": %s,\n",
+			   ec->health_failure & JENT_APT_FAILURE ? "true" : "false");
+	jent_add_to_status("\t\t\t\"permanent\": %s\n",
+			   ec->health_failure & JENT_APT_FAILURE_PERMANENT ? "true" : "false");
+	jent_add_to_status("\t\t},\n");
+
+	jent_add_to_status("\t\t\"rct\": {\n");
+	jent_add_to_status("\t\t\t\"intermittent\": %s,\n",
+			   ec->health_failure & JENT_RCT_FAILURE ? "true" : "false");
+	jent_add_to_status("\t\t\t\"permanent\": %s\n",
+			   ec->health_failure & JENT_RCT_FAILURE_PERMANENT ? "true" : "false");
+	jent_add_to_status("\t\t}");
+
 #ifdef JENT_HEALTH_LAG_PREDICTOR
-	used = strlen(buf);
-	snprintf(buf + used, buflen - used,
-		 " LAG:\n  intermittent: %s\n  permanent: %s\n",
-		 ec->health_failure & JENT_LAG_FAILURE ? "fail" : "pass",
-		 ec->health_failure & JENT_LAG_FAILURE_PERMANENT ? "fail" :
-								   "pass");
+	jent_add_to_status(",\n");
+
+	jent_add_to_status("\t\t\"lag\": {\n");
+	jent_add_to_status("\t\t\t\"intermittent\": %s,\n",
+			   ec->health_failure & JENT_LAG_FAILURE ? "true" : "false");
+	jent_add_to_status("\t\t\t\"permanent\": %s\n",
+			   ec->health_failure & JENT_LAG_FAILURE_PERMANENT ? "true" : "false");
+	jent_add_to_status("\t\t}\n");
+#else
+	jent_add_to_status("\n");
 #endif
 
-	used = strlen(buf);
-	snprintf(buf + used, buflen - used, "Configuration:\n");
+	jent_add_to_status("\t},\n");
 
-	used = strlen(buf);
-	snprintf(buf + used, buflen - used, " OSR: %u\n", ec->osr);
+	/*
+	 * runtime environment
+	 */
+	jent_add_to_status( "\t\"runtimeEnvironment\": {\n");
 
-	used = strlen(buf);
-	snprintf(buf + used, buflen - used, " Memory Block Size: %u bytes\n",
-		 jent_memsize(ec->flags));
+	jent_add_to_status("\t\t\"cpuCores\": %ld,\n", jent_ncpu());
 
-	used = strlen(buf);
-	snprintf(buf + used, buflen - used,
-		 " Detected Cache Sizes:\n  L1 %u bytes\n  all caches %u bytes\n",
-		 jent_cache_size_roundup(0), jent_cache_size_roundup(1));
+	jent_add_to_status("\t\t\"cpuCache\": {\n");
+	jent_add_to_status("\t\t\t\"l1Bytes\": %u,\n", jent_cache_size_roundup(0));
+	jent_add_to_status("\t\t\t\"allBytes\": %u\n", jent_cache_size_roundup(1));
+	jent_add_to_status("\t\t}\n");
+	jent_add_to_status("\t},\n");
 
-	used = strlen(buf);
-	snprintf(buf + used, buflen - used,
-		 " Hash loop count:\n  runtime: %u\n  initialization: %u\n",
-		 jent_hashloop_cnt(ec->flags),
-		 jent_hashloop_cnt(ec->flags) * JENT_HASH_LOOP_INIT);
+	/*
+	 * configuration
+	 */
+	jent_add_to_status( "\t\"configuration\": {\n");
 
-	used = strlen(buf);
-	snprintf(buf + used, buflen - used,
-		 " Memory Access loop count:\n  runtime: %u\n  initialization: %u\n",
-		 ec->memaccessloops,
-		 ec->memaccessloops * JENT_MEM_ACC_LOOP_INIT);
+	jent_add_to_status( "\t\t\"osr\": %u,\n", ec->osr);
+	jent_add_to_status( "\t\t\"memoryBlockSizeBytes\": %u,\n", jent_memsize(ec->flags));
 
-	used = strlen(buf);
-	snprintf(buf + used, buflen - used,
-		 " Secure Memory: %i\n", jent_secure_memory_supported());
+	jent_add_to_status("\t\t\"hashLoopCount\": {\n");
+	jent_add_to_status("\t\t\t\"runtime\": %u,\n", jent_hashloop_cnt(ec->flags));
+	jent_add_to_status("\t\t\t\"initialization\": %u\n", jent_hashloop_cnt(ec->flags) * JENT_HASH_LOOP_INIT);
+	jent_add_to_status("\t\t},\n");
 
-	used = strlen(buf);
-	snprintf(buf + used, buflen - used,
-		 " Internal Timer: %i\n", ec->enable_notime);
+	jent_add_to_status("\t\t\"memoryLoopCount\": {\n");
+	jent_add_to_status("\t\t\t\"runtime\": %u,\n", ec->memaccessloops);
+	jent_add_to_status("\t\t\t\"initialization\": %u\n", ec->memaccessloops * JENT_MEM_ACC_LOOP_INIT);
+	jent_add_to_status("\t\t},\n");
 
-	used = strlen(buf);
-	snprintf(buf + used, buflen - used,
-		 " FIPS mode: %i\n", ec->fips_enabled);
+	jent_add_to_status("\t\t\"secureMemory\": %s,\n", jent_secure_memory_supported() ? "true" : "false");
+	jent_add_to_status("\t\t\"internalTimer\": %s,\n", ec->enable_notime ? "true" : "false");
+	jent_add_to_status("\t\t\"fipsMode\": %s,\n", ec->fips_enabled ? "true" : "false");
+	jent_add_to_status("\t\t\"ntg1Mode\": %s,\n", !!(ec->flags & JENT_NTG1) ? "true" : "false");
 
-	used = strlen(buf);
-	snprintf(buf + used, buflen - used,
-		 " AIS 20/31 NTG.1 mode: %i\n", !!(ec->flags & JENT_NTG1));
+	jent_add_to_status("\t\t\"flags\": {\n");
+	jent_add_to_status("\t\t\t\"JENT_DISABLE_MEMORY_ACCESS\": %s,\n",
+		 !!(ec->flags & JENT_DISABLE_MEMORY_ACCESS) ? "true" : "false");
+	jent_add_to_status("\t\t\t\"JENT_FORCE_INTERNAL_TIMER\": %s,\n",
+		 !!(ec->flags & JENT_FORCE_INTERNAL_TIMER) ? "true" : "false");
+	jent_add_to_status("\t\t\t\"JENT_DISABLE_INTERNAL_TIMER\": %s,\n",
+		 !!(ec->flags & JENT_DISABLE_INTERNAL_TIMER) ? "true" : "false");
+	jent_add_to_status("\t\t\t\"JENT_FORCE_FIPS\": %s,\n",
+		 !!(ec->flags & JENT_FORCE_FIPS) ? "true" : "false");
+	jent_add_to_status("\t\t\t\"JENT_NTG1\": %s,\n",
+		 !!(ec->flags & JENT_NTG1) ? "true" : "false");
+	jent_add_to_status("\t\t\t\"JENT_CACHE_ALL\": %s\n",
+		 !!(ec->flags & JENT_CACHE_ALL) ? "true" : "false");
+	jent_add_to_status("\t\t}\n");
+	jent_add_to_status("\t}\n");
 
-	used = strlen(buf);
-	snprintf(buf + used, buflen - used,
-		 " CPU Cores: %ld\n",
-		 jent_ncpu());
-
-	used = strlen(buf);
-	snprintf(buf + used, buflen - used, " Flags:\n");
-
-	used = strlen(buf);
-	snprintf(buf + used, buflen - used,
-		 "  JENT_DISABLE_MEMORY_ACCESS %u\n",
-		 !!(ec->flags & JENT_DISABLE_MEMORY_ACCESS));
-
-	used = strlen(buf);
-	snprintf(buf + used, buflen - used,
-		 "  JENT_FORCE_INTERNAL_TIMER %u\n",
-		 !!(ec->flags & JENT_FORCE_INTERNAL_TIMER));
-
-	used = strlen(buf);
-	snprintf(buf + used, buflen - used,
-		 "  JENT_DISABLE_INTERNAL_TIMER %u\n",
-		 !!(ec->flags & JENT_DISABLE_INTERNAL_TIMER));
-
-	used = strlen(buf);
-	snprintf(buf + used, buflen - used,
-		 "  JENT_FORCE_FIPS %u\n",
-		 !!(ec->flags & JENT_FORCE_FIPS));
-
-	used = strlen(buf);
-	snprintf(buf + used, buflen - used, "  JENT_NTG1 %u\n",
-		 !!(ec->flags & JENT_NTG1));
-
-	used = strlen(buf);
-	snprintf(buf + used, buflen - used,
-		 "  JENT_CACHE_ALL %u\n",
-		 !!(ec->flags & JENT_CACHE_ALL));
+	jent_add_to_status("}\n");
 
 	return 0;
+#undef jent_add_to_status
 }
