b0a0e3628e25e4212ef328f557cfe23937f4616b
Stephan Mueller <smueller@chronox.de>
2026-07-12 17:49:56 +0200

    Linux kernel: compile KCAPI support conditionally
    Signed-off-by: Stephan Mueller <smueller@chronox.de>

--- linux_kernel/jitterentropy_kcapi.h
+++ linux_kernel/jitterentropy_kcapi.h
@@ -8,7 +8,22 @@
 #ifndef _JITTERENTROPY_KCAPI_H
 #define _JITTERENTROPY_KCAPI_H
 
+#ifdef CONFIG_EXTERNAL_JITTERENTROPY_KCAPI
+
 int jent_kcapi_init(void);
 void jent_kcapi_exit(void);
+
+#else /* CONFIG_EXTERNAL_JITTERENTROPY_KCAPI */
+
+static inline int jent_kcapi_init(void)
+{
+	return 0;
+}
+
+static inline void jent_kcapi_exit(void)
+{
+}
+
+#endif /* CONFIG_EXTERNAL_JITTERENTROPY_KCAPI */
 
 #endif /* _JITTERENTROPY_KCAPI_H */
