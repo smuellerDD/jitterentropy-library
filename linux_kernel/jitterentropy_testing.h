6b7cb22aa55b708272fb381deb11f766847a56be
Markus Theil <theil.markus@gmail.com>
2026-07-22 15:42:33 +0200

    kmod: check proc and testing return values
    Signed-off-by: Markus Theil <theil.markus@gmail.com>
    Signed-off-by: Stephan Mueller <smueller@chronox.de>

--- linux_kernel/jitterentropy_testing.h
+++ linux_kernel/jitterentropy_testing.h
@@ -11,10 +11,15 @@
 #ifdef CONFIG_EXTERNAL_JITTERENTROPY_TESTINTERFACE
 #include <linux/init.h>
 
-void __init jent_testing_init(void);
+/*
+ * Create/remove the debugfs test interface. A creation failure is returned
+ * as an error after removing anything already created; a kernel without
+ * debugfs or with lockdown active skips the interface and succeeds.
+ */
+int __init jent_testing_init(void);
 void jent_testing_exit(void);
 #else /* CONFIG_EXTERNAL_JITTERENTROPY_TESTINTERFACE */
-static inline void jent_testing_init(void) { }
+static inline int jent_testing_init(void) { return 0; }
 static inline void jent_testing_exit(void) { }
 #endif /* CONFIG_EXTERNAL_JITTERENTROPY_TESTINTERFACE */
 
